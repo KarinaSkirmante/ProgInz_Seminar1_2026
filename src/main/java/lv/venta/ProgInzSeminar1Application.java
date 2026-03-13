239870fea48c1514dfcd1cfd8330c7b65502eed9
KarinaSkirmante <karina.krinkele@gmail.com>
2026-03-13 18:21:41 +0200

    Edit: play with repo functions

--- src/main/java/lv/venta/ProgInzSeminar1Application.java
+++ src/main/java/lv/venta/ProgInzSeminar1Application.java
@@ -4,13 +4,22 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+import lv.venta.controller.SimpleController;
 import lv.venta.model.Category;
 import lv.venta.model.Product;
 import lv.venta.repo.IProductRepo;
 
 @SpringBootApplication
 public class ProgInzSeminar1Application {
+
+    private final SimpleController simpleController;
+
+    private final IProductRepo IProductRepo;
+
+    ProgInzSeminar1Application(IProductRepo IProductRepo, SimpleController simpleController) {
+        this.IProductRepo = IProductRepo;
+        this.simpleController = simpleController;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgInzSeminar1Application.class, args);
@@ -22,6 +31,10 @@ public class ProgInzSeminar1Application {
 			
 			@Override
 			public void run(String... args) throws Exception {
+				
+				//CrudRepositoriju funkcijas: //https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/CrudRepository.html
+
+				
 				Product p1 = new Product("Ābols", 0.99f, "Salds un garšīgs", Category.fruit, 10);
 				Product p2 = new Product("Burkāns", 2.84f, "Oranžš", Category.vegetable, 40);
 				Product p3 = new Product("Vīnogas", 4.99f, "Violetas", Category.fruit, 3);
@@ -30,12 +43,18 @@ public class ProgInzSeminar1Application {
 				prodRepo.save(p2);
 				prodRepo.save(p3);
 				
-				//https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/CrudRepository.html
-				//TODO
-				//caur prodRepo atrast produktu, kam id ir 1
-				//caur prodRepo, izdzest produktu, kam id ir 1
-				//caur prodrepo atrast visus produktus, kas ir tabula
-				//caur prodrepo dabut produktu ar id 2 un tam nomainit cenu un ar save saglabat
+				
+				System.out.println("Ierakstu skaits: " +prodRepo.count());
+				System.out.println("Produkts ar id=1: " + prodRepo.findById(1).get());
+				System.out.println("Visi produkti: " + prodRepo.findAll());
+				prodRepo.deleteById(1);
+				System.out.println("Visi produkti pec 1.dzešanas" + prodRepo.findAll());
+				
+				Product productForUpdate = prodRepo.findById(2).get();
+				productForUpdate.setPrice(9.99f);
+				prodRepo.save(productForUpdate);
+				System.out.println("Visi produkti pec 1.dzešanas un 2.rediģēšanas" + prodRepo.findAll());
+				
 				
 			}
 		};
