e6e40ab2523525e01b4f26eb7082108bb7e1911f
KarinaSkirmante <karina.krinkele@gmail.com>
2026-03-13 19:10:36 +0200

    Add: added CRUDPRoduct service implementation - retrieve and create

--- src/main/java/lv/venta/repo/IProductRepo.java
+++ src/main/java/lv/venta/repo/IProductRepo.java
@@ -6,4 +6,7 @@ import lv.venta.model.Product;
 
 public interface IProductRepo extends CrudRepository<Product, Integer>{
 
+	//te automatiski izveidosies vaicājums: SELECT * FROM PRODUCT_TABLE WHERE TITLE=title, bet atgriezīs true, ja būs kādi dati
+	public abstract boolean existsByTitle(String title);
+
 }
